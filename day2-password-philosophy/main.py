f23bb0073f786499ef1214b491ab84a825276b2e
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2020-12-02 14:35:29 +0200

    Day2, part 2 solved

--- day2-password-philosophy/main.py
+++ day2-password-philosophy/main.py
@@ -8,19 +8,36 @@ def read_input():
         return input_data
 
 
-def is_valid(entry):
+def is_valid_with_old_policy(entry):
     limit, character, password = entry.replace(":", "").split(" ")
     lower_limit, upper_limit = limit.split("-")
     return int(lower_limit) <= password.count(character) <= int(upper_limit)
 
 
-def solve_part_1():
+def check_valid_passwords(validation_method):
     valid_passwords = 0
     for entry_line in read_input():
-        if is_valid(entry_line):
+        if validation_method(entry_line):
             valid_passwords += 1
     print(f"Valid passwords {valid_passwords}")
 
 
+def solve_part_1():
+    check_valid_passwords(is_valid_with_old_policy)
+
+
+def is_valid_with_new_policy(entry):
+    positions, character, password = entry.replace(":", "").split(" ")
+    pos_a, pos_b = positions.split("-")
+    pos_a_contains_char = password[int(pos_a) - 1] == character
+    pos_b_contains_char = password[int(pos_b) - 1] == character
+    return bool(pos_a_contains_char) != bool(pos_b_contains_char)
+
+
+def solve_part_2():
+    check_valid_passwords(is_valid_with_new_policy)
+
+
 if __name__ == '__main__':
     solve_part_1()
+    solve_part_2()
