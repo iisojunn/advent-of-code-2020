a0359d4463e675a476304dd9410208b7bb2a29ae
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2020-12-04 10:59:07 +0200

    Day 4 task 1 ready

---
+++ day4-passport-protection/main.py
@@ -0,0 +1,33 @@
+"""Day 4 advent of code 2020"""
+import re
+
+REQUIRED_FIELDS = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+
+
+def passport_dict(chunk):
+    fields = chunk.replace("\n", " ").rstrip().split(" ")
+    return {k: v for k, v in [field.split(":") for field in fields]}
+
+
+def read_input():
+    with open("input", "r") as input_file:
+        data = input_file.read()
+        return [passport_dict(chunk) for chunk in re.split("\n\n", data)]
+
+
+def is_valid(passport):
+    return REQUIRED_FIELDS.issubset(set(passport.keys()))
+
+
+def count_valid(passports):
+    valid = 0
+    for passport in passports:
+        if is_valid(passport):
+            valid += 1
+    return valid
+
+
+if __name__ == '__main__':
+    passport_info = read_input()
+    print(f"All passport info dicts: {passport_info}")
+    print(f"Valid passports {count_valid(passport_info)}")
