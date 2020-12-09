3d5268350f2f6e528413d08662b241c138e177dd
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2020-12-09 20:01:13 +0200

    Day 5 task 1 ready

---
+++ day5-binary-boarding/main.py
@@ -0,0 +1,38 @@
+"""Day 5 advent of code 2020"""
+
+
+def read_input():
+    with open("input", "r") as input_data:
+        return input_data.read().splitlines()
+
+
+def fb_to_01(binary_row):
+    return binary_row.replace("F", "0").replace("B", "1")
+
+
+def row_number(binary_row):
+    values = [64, 32, 16, 8, 4, 2, 1]
+    return sum([int(binary) * value for binary, value
+                in zip(binary_row, values)])
+
+
+def lr_to_01(binary_column):
+    return binary_column.replace("L", "0").replace("R", "1")
+
+
+def column_number(binary_column):
+    values = [4, 2, 1]
+    return sum([int(binary) * value for binary, value
+                in zip(binary_column, values)])
+
+
+def calculate_seat_id(boarding_pass):
+    return row_number(fb_to_01(boarding_pass[:7])) * 8 \
+           + column_number(lr_to_01(boarding_pass[7:]))
+
+
+if __name__ == '__main__':
+    seat_ids = [calculate_seat_id(boarding_pass) for boarding_pass in
+                read_input()]
+    seat_ids.sort(reverse=True)
+    print(f"Highest seat_id {seat_ids[0]}")
