def3497311d87635fcd3e77e30c1c5c31d37e860
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2020-12-10 13:55:47 +0200

    Day 6 task 2 ready

--- day6-custom-customs/main.py
+++ day6-custom-customs/main.py
@@ -6,18 +6,28 @@ def read_input():
         return input_file.read().splitlines()
 
 
-def group_answers(input_list):
-    answers = set()
-    for row in input_list:
+def group_answers():
+    group = []
+    for row in read_input():
         if row:
-            answers = answers.union(set(row))
+            group.append(set(row))
         else:
-            yield answers
-            answers = set()
-    if answers:
-        yield answers
+            yield group
+            group = []
+    if group:
+        yield group
+
+
+def any_yes_answers():
+    return [answers[0].union(*answers) for answers in group_answers()]
+
+
+def all_yes_answers():
+    return [answers[0].intersection(*answers) for answers in group_answers()]
 
 
 if __name__ == '__main__':
-    any_yes = sum([len(answers) for answers in group_answers(read_input())])
+    any_yes = sum([len(answers) for answers in any_yes_answers()])
     print(f"Sum of any yes answers by groups is {any_yes}")
+    all_yes = sum([len(answers) for answers in all_yes_answers()])
+    print(f"Sum of all yes answers by groups is {all_yes}")
