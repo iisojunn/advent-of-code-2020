eab7d6e3ee5aa818354c1f1003510cd8f3377149
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2020-12-14 20:00:48 +0200

    Day 10 task 2 solved

--- day10-adapter-array/main.py
+++ day10-adapter-array/main.py
@@ -1,4 +1,6 @@
 """Day 9 Advent of code 2020"""
+import math
+
 from more_itertools import quantify
 
 
@@ -21,9 +23,31 @@ def count_steps(step_list, step_size):
     return quantify(step_list, lambda step: step == step_size)
 
 
+def lengths_of_one_rows(step_list):
+    rows_of_ones = []
+    found_ones = 0
+    for step in step_list:
+        if step == 1:
+            found_ones += 1
+        elif step != 1 and found_ones > 0:
+            rows_of_ones.append(found_ones)
+            found_ones = 0
+    return rows_of_ones
+
+
+def calculate_permutations(one_rows):
+    possible_permutations = {0: 1, 1: 1, 2: 2, 3: 4, 4: 7}
+    factors = [possible_permutations[x] for x in one_rows]
+    return math.prod(factors)
+
+
 if __name__ == '__main__':
+    print(adapter_list())
     STEPS = steps(adapter_list())
     ones = count_steps(STEPS, step_size=1)
     threes = count_steps(STEPS, step_size=3)
     print(f"The result of one steps and three steps multiplication is "
           f"{ones * threes}")
+    ONE_ROWS = lengths_of_one_rows(STEPS)
+    permutations = calculate_permutations(ONE_ROWS)
+    print(f"Total number of adapter connection permutations {permutations}")
