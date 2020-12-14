345589d33115faa89b028df704af9f3a585097d0
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2020-12-14 11:36:18 +0200

    Day 8 task 2 done

--- day8-handheld-halting/main.py
+++ day8-handheld-halting/main.py
@@ -35,7 +35,49 @@ def execute_until_second_loop_starts(code):
     return acc
 
 
+class LastCommandNotReached(Exception):
+    pass
+
+
+def execute_until_last_command(code):
+    acc = 0
+    code_index = 0
+    last_index = len(code)
+    visited = []
+    while code_index not in visited:
+        visited.append(code_index)
+        acc, code_index = execute(code[code_index], acc, code_index)
+        if code_index == last_index:
+            return acc
+    raise LastCommandNotReached
+
+
+def create_candidate(code, index, operation, new_operation):
+    candidate = code.copy()
+    candidate[index] = code[index].replace(operation, new_operation)
+    return candidate
+
+
+def code_candidates(code):
+    for i, instruction in enumerate(code):
+        if "jmp" in instruction:
+            yield create_candidate(code, i, "jmp", "nop")
+        elif "nop" in instruction:
+            yield create_candidate(code, i, "nop", "jmp")
+
+
+def fix_loop_and_execute(code):
+    for code_candidate in code_candidates(code):
+        try:
+            acc = execute_until_last_command(code_candidate)
+            return acc
+        except LastCommandNotReached:
+            continue
+
+
 if __name__ == '__main__':
     CODE = read_input()
     accumulator = execute_until_second_loop_starts(CODE)
     print(f"Accumulator value before second visit is {accumulator}")
+    accumulator = fix_loop_and_execute(CODE)
+    print(f"Accumulator value with fixed loop is {accumulator}")
