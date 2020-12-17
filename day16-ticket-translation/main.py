0203bdab25f4817c90152b40870e5bae36f4ccc6
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2020-12-17 14:23:50 +0200

    Day 16 task 2 done

--- day16-ticket-translation/main.py
+++ day16-ticket-translation/main.py
@@ -1,4 +1,5 @@
 """Day 16 Advent of code"""
+import math
 
 
 def read_input():
@@ -25,9 +26,13 @@ def is_in_range(field, range_):
     return range_[0] <= field <= range_[1]
 
 
+def is_in_any_range(field, ranges):
+    return any([is_in_range(field, range_) for range_ in ranges])
+
+
 def is_valid(field, field_ranges):
     for ranges in field_ranges.values():
-        if any([is_in_range(field, range_) for range_ in ranges]):
+        if is_in_any_range(field, ranges):
             return True
     return False
 
@@ -41,8 +46,58 @@ def scan_tickets(field_ranges, tickets):
     return error_rate
 
 
+def is_all_in_range(ranges, field_values):
+    return all([is_in_any_range(field, ranges) for field in field_values])
+
+
+def resolve_possible_field_orders(valid_tickets, field_ranges):
+    possible_field_indices = {key: [] for key in field_ranges.keys()}
+    for index, values in enumerate(transpose(valid_tickets)):
+        for field_name, ranges in field_ranges.items():
+            if is_all_in_range(ranges, values):
+                possible_field_indices[field_name].append(index)
+    return possible_field_indices
+
+
+def transpose(valid_tickets):
+    return list(map(list, zip(*valid_tickets)))
+
+
+def only_option(possible_field_order):
+    for name, possible_indices in possible_field_order.items():
+        if len(possible_indices) == 1:
+            return name, possible_indices[0]
+
+
+def resolve_field_order(valid_tickets, field_ranges):
+    field_order = {}
+    possible = resolve_possible_field_orders(valid_tickets, field_ranges)
+    while len(possible) != len(field_order):
+        field_name, index = only_option(possible)
+        field_order[field_name] = index
+        consume_index(index, possible)
+    return field_order
+
+
+def consume_index(index, possible):
+    for indices in possible.values():
+        if index in indices:
+            indices.pop(indices.index(index))
+
+
+def multiply_departure_fields(my_ticket, field_order):
+    return math.prod([my_ticket[index] for field, index in field_order.items()
+                      if field.startswith("departure")])
+
+
 if __name__ == '__main__':
     DATA = read_input()
     FIELD_RANGES, MY_TICKET, TICKETS = parse_input(DATA)
     ERROR_RATE = scan_tickets(FIELD_RANGES, TICKETS)
     print(f"Ticket scanning error rate is {ERROR_RATE}")
+
+    VALID_TICKETS = [ticket for ticket in TICKETS if
+                     all([is_valid(field, FIELD_RANGES) for field in ticket])]
+    FIELD_ORDER = resolve_field_order(VALID_TICKETS, FIELD_RANGES)
+    result = multiply_departure_fields(MY_TICKET, FIELD_ORDER)
+    print(f"My ticket six departure values multiplied is {result}")
