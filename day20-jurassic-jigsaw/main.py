98defba64e5ee75b465816bd3afae15404249388
Ilari Iso-Junno <ilari.isojunno@gmail.com>
2020-12-29 15:12:48 +0200

    Day 20 task 2 done

--- day20-jurassic-jigsaw/main.py
+++ day20-jurassic-jigsaw/main.py
@@ -1,36 +1,86 @@
 """Day 20 Advent of code"""
 import math
-import numpy
+import re
 
+import numpy
 from itertools import product
+
+LEFT = "left_side"
+DOWN = "downside"
+RIGHT = "right_side"
+UP = "upside"
+
+SIDES = {
+    (0, -1): UP,
+    (1, 0): RIGHT,
+    (0, 1): DOWN,
+    (-1, 0): LEFT,
+}
+
+OPPOSITES = {
+    UP: DOWN,
+    RIGHT: LEFT,
+    DOWN: UP,
+    LEFT: RIGHT
+}
+
+MONSTER = r"""..................#.
+#....##....##....###
+.#..#..#..#..#..#..."""
 
 
 class Tile:
 
     def __init__(self, id_, rows):
         self.id = int(id_)
-        self.data = [list(row) for row in rows]
-        self.upside = "".join(self.data[0])
-        self.downside = "".join(self.data[-1])
-        self.left_side = "".join([row[0] for row in self.data])
-        self.right_side = "".join([row[-1] for row in self.data])
-        self.matches = set()
+        self.data = numpy.array([list(row) for row in rows])
+        self.matches = {}
 
     def __repr__(self):
         return f"<Tile {self.id}>"
 
     def __str__(self):
-        data = "\n".join("".join(row)for row in self.data)
-        return f'Tile {self.id}:\n{data}\n'
+        return f'Tile {self.id}:\n{self.data_str()}\n'
 
-    def transpose(self):
-        self.upside, self.left_side = self.left_side, self.upside
-        self.right_side, self.downside = self.downside, self.right_side
+    def data_str(self):
+        return "\n".join("".join(row) for row in self.data)
+
+    @property
+    def upside(self):
+        return "".join(self.data[0])
+
+    @property
+    def right_side(self):
+        return "".join([row[-1] for row in self.data])
+
+    @property
+    def downside(self):
+        return "".join(self.data[-1])
+
+    @property
+    def left_side(self):
+        return "".join([row[0] for row in self.data])
+
+    def flip(self):
+        self.data = numpy.flipud(self.data)
+        self.change_match_sides([(UP, DOWN),
+                                 (RIGHT, RIGHT),
+                                 (DOWN, UP),
+                                 (LEFT, LEFT)])
 
     def rotate(self):
-        self.upside, self.right_side, self.downside, self.left_side = \
-            self.right_side, "".join(reversed(self.downside)), \
-            self.left_side, "".join(reversed(self.upside))
+        self.data = numpy.rot90(self.data)
+        self.change_match_sides([(UP, LEFT),
+                                 (RIGHT, UP),
+                                 (DOWN, RIGHT),
+                                 (LEFT, DOWN)])
+
+    def change_match_sides(self, sides):
+        matches = self.matches.copy()
+        self.matches = {}
+        for side, new_side in sides:
+            if matches.get(side):
+                self.matches[new_side] = matches.get(side)
 
     def sides(self):
         return self.upside, self.right_side, self.downside, self.left_side
@@ -41,9 +91,28 @@ class Tile:
     def all_sides(self):
         return *self.sides(), *self.reversed_sides()
 
+    def check_matches(self, other):
+        for side in [UP, DOWN, LEFT, RIGHT]:
+            if getattr(self, side) in other.all_sides():
+                self.matches[side] = other
 
-def read_input():
-    with open("input", "r") as input_file:
+    def flip_and_rotate_to_match(self, other, side):
+        for _flip in range(2):
+            for _rotation in range(4):
+                if self.sides_match(other, side):
+                    return
+                self.rotate()
+            self.flip()
+
+    def sides_match(self, other, side):
+        return getattr(self, side) == getattr(other, OPPOSITES[side])
+
+    def image_data(self):
+        return [row[1:-1] for row in self.data[1:-1]]
+
+
+def read_input(filename):
+    with open(filename, "r") as input_file:
         return [parse_tile(tile) for tile in input_file.read().split("\n\n")]
 
 
@@ -53,104 +122,144 @@ def parse_tile(tile):
     return Tile(tile_id, tile_data[1:])
 
 
-def spots_for_tile(image, new_tile, neighbour_required):
-    for spot in possible_spots(image, neighbour_required):
-        for _transpose in range(2):
-            for _rotation in range(4):
-                if is_suitable_spot(spot, new_tile, image):
-                    yield spot, new_tile
-                new_tile.rotate()
-            new_tile.transpose()
-
-
-def possible_spots(image, neighbour_required):
-    for spot, tile in image.items():
-        if tile is None:
-            if not neighbour_required or has_neighbour(image, spot):
-                yield spot
-
-
-def has_neighbour(image, spot):
-    x, y = spot
-    for x0, y0 in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
-        try:
-            neighbour = image[(x0, y0)]
-        except KeyError:
-            pass
-        else:
-            if neighbour is not None:
-                return True
-    return False
-
-
-def is_suitable_spot(spot, new_tile, image):
-    matching_sides = [is_suitable_side(new_tile, side, neighbour_pixels) for
-                      side, neighbour_pixels in sides_to_compare(image, spot)]
-    return all(matching_sides)
-
-
-def is_suitable_side(tile, side, neighbour_pixels):
-    return getattr(tile, side) == neighbour_pixels
-
-
-def sides_to_compare(image, spot):
-    x, y = spot
-    for my_side, neighbour_side, x0, y0 in adjacent(x, y):
-        try:
-            neighbour = image[(x0, y0)]
-        except KeyError:
-            pass
-        else:
-            if neighbour is not None:
-                yield my_side, getattr(neighbour, neighbour_side)
-
-
-def adjacent(x, y):
-    yield "left_side", "right_side", x - 1, y
-    yield "right_side", "left_side", x + 1, y
-    yield "upside", "downside", x, y - 1
-    yield "downside", "upside", x, y + 1
-
-
-def base_image(tiles):
-    return {(x, y): None for x, y in
-            product(range(int(numpy.math.sqrt(tiles))), repeat=2)}
-
-
-def is_filled(image):
-    return all(tile is not None for tile in image.values())
-
-
-def place_tiles(image, tiles, neighbour_required=False):
-    check = [i for i, tile in enumerate(tiles) if tile not in image.values()]
-    while check:
-        tile = tiles[check.pop()]
-        for spot, tile0 in spots_for_tile(image, tile, neighbour_required):
-            image[spot] = tile0
-            image = place_tiles(image, tiles, True)
-            if is_filled(image):
-                return image
-            image[spot] = None
-    return image
-
-
 def search_matches(tiles):
-    for tile in tiles:
-        for other in tiles:
-            if tile.id != other.id:
-                for side in tile.all_sides():
-                    if side in other.all_sides():
-                        tile.matches.add(other)
+    for tile, other in product(tiles, repeat=2):
+        if tile.id != other.id:
+            tile.check_matches(other)
 
 
 def search_corners(tiles):
     return [tile for tile in tiles if len(tile.matches) == 2]
 
 
+def base_image(size):
+    return {(x, y): None for x, y in product(range(size), repeat=2)}
+
+
+def place_first_tile(image, corner):
+    image[(0, 0)] = corner
+    while {DOWN, RIGHT} != set(corner.matches.keys()):
+        corner.rotate()
+    return image
+
+
+def place_tiles(image):
+    for empty_spot, neighbour_spot_, neighbour in spots_to_fill(image):
+        side = get_side(empty_spot, neighbour_spot_)
+        tile = neighbour.matches[OPPOSITES[side]]
+        image[empty_spot] = tile
+        tile.flip_and_rotate_to_match(neighbour, side)
+    return image
+
+
+def spots_to_fill(image):
+    while not is_filled(image):
+        for empty_spot in empty_spots(image):
+            for spot, neighbour in neighbour_spot(image, empty_spot):
+                yield empty_spot, spot, neighbour
+
+
+def empty_spots(image):
+    yield from {spot for spot, tile in image.items() if tile is None}
+
+
+def is_filled(image):
+    return all(tile is not None for tile in image.values())
+
+
+def neighbour_spot(image, spot):
+    x, y = spot
+    for spot_ in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+        try:
+            neighbour = image.get(spot_)
+        except KeyError:
+            pass
+        else:
+            if neighbour is not None:
+                yield spot_, neighbour
+                break
+
+
+def get_side(origin, destination):
+    return SIDES[subtract(destination, origin)]
+
+
+def subtract(tuple1, tuple2) -> tuple:
+    return tuple(a - b for a, b in zip(tuple1, tuple2))
+
+
+def side_length(square):
+    return int(numpy.math.sqrt(len(square)))
+
+
+def original_image(tiles, corner_tiles):
+    image_size = side_length(tiles)
+    image = create_image(corner_tiles, image_size)
+    return Tile(1, image_list(combined_image(image, image_size)))
+
+
+def create_image(corner_tiles, image_size):
+    image = base_image(image_size)
+    image = place_first_tile(image, corner_tiles[0])
+    image = place_tiles(image)
+    return image
+
+
+def combined_image(image, size):
+    combined = {}
+    for y in range(size):
+        for x in range(size):
+            data = image[(x, y)].image_data()
+            for y0, row in enumerate(data):
+                for x0, cell in enumerate(row):
+                    combined[(x * len(data) + x0,
+                              y * len(data[0]) + y0)] = cell
+    return combined
+
+
+def image_list(image_dict):
+    image = []
+    for y in range(side_length(image_dict)):
+        image.append([])
+        for x in range(side_length(image_dict)):
+            image[y].append(image_dict[(x, y)])
+    return image
+
+
+def monster_areas(image, monster):
+    monster_ = monster.splitlines()
+    for y in range(0, len(image) - len(monster_)):
+        for x in range(0, len(image[0]) - len(monster_[0])):
+            sub_area = image[y:y + len(monster_), x:x + len(monster_[0])]
+            sub_string = "\n".join(["".join(row) for row in sub_area])
+            yield sub_string
+
+
+def sea_monster_count(image, monster):
+    return sum([monster_count(data, monster) for data in orientations(image)])
+
+
+def monster_count(image, monster):
+    return [True for string in monster_areas(image, monster) if
+            re.match(monster, string)].count(True)
+
+
+def orientations(image_tile):
+    for _flip in range(2):
+        for _rotation in range(4):
+            yield image_tile.data
+            image_tile.rotate()
+        image_tile.flip()
+
+
 if __name__ == '__main__':
-    TILES = read_input()
-    print(TILES)
+    TILES = read_input("input")
     search_matches(TILES)
-    corner_tiles = search_corners(TILES)
-    result = math.prod([tile.id for tile in corner_tiles])
-    print(f"Corner tiles multiplied is {result}")
+    CORNER_TILES = search_corners(TILES)
+    RESULT = math.prod([TILE.id for TILE in CORNER_TILES])
+    print(f"Corner tiles multiplied is {RESULT}")
+
+    IMAGE = original_image(TILES, CORNER_TILES)
+    MONSTERS = sea_monster_count(IMAGE, MONSTER)
+    ROUGHNESS = IMAGE.data_str().count("#") - MONSTERS * MONSTER.count("#")
+    print(f"Sea roughness is {ROUGHNESS}")
--- day20-jurassic-jigsaw/test.py
+++ day20-jurassic-jigsaw/test.py
@@ -1,35 +1,38 @@
 """Tests"""
 import unittest
 
-from main import Tile
+import numpy
+
+from main import Tile, UP, RIGHT, DOWN, LEFT, sea_monster_count, MONSTER, \
+    read_input, search_matches, search_corners, original_image, create_image
 
 
 class TestTile(unittest.TestCase):
 
     def setUp(self):
-        self.tile = Tile(1, ["..#", "###", ".##"])
+        self.tile = Tile(1, ["..#.", "##.#", "....", "#.#."])
 
     def test_upside(self):
-        self.assertEqual(self.tile.upside, "..#")
-
-    def test_downside(self):
-        self.assertEqual(self.tile.downside, ".##")
+        self.assertEqual(self.tile.upside, "..#.")
 
     def test_right_side(self):
-        self.assertEqual(self.tile.right_side, "###")
+        self.assertEqual(self.tile.right_side, ".#..")
+
+    def test_downside(self):
+        self.assertEqual(self.tile.downside, "#.#.")
 
     def test_left_side(self):
-        self.assertEqual(self.tile.left_side, ".#.")
+        self.assertEqual(self.tile.left_side, ".#.#")
 
-    def test_transpose(self):
+    def test_flip(self):
         upside, right_side, downside, left_side = self.tile.sides()
-        self.tile.transpose()
-        self.assertEqual(self.tile.upside, left_side)
-        self.assertEqual(self.tile.right_side, downside)
-        self.assertEqual(self.tile.downside, right_side)
-        self.assertEqual(self.tile.left_side, upside)
+        self.tile.flip()
+        self.assertEqual(self.tile.upside, downside)
+        self.assertEqual(self.tile.right_side, "".join(reversed(right_side)))
+        self.assertEqual(self.tile.downside, upside)
+        self.assertEqual(self.tile.left_side, "".join(reversed(left_side)))
 
-    def test_rotate(self):
+    def test_rotate_sides(self):
         upside, right_side, downside, left_side = self.tile.sides()
         self.tile.rotate()
         self.assertEqual(self.tile.upside, right_side)
@@ -37,20 +40,126 @@ class TestTile(unittest.TestCase):
         self.assertEqual(self.tile.downside, left_side)
         self.assertEqual(self.tile.left_side, "".join(reversed(upside)))
 
+    def test_rotate_changes_matches(self):
+        self.tile.matches = {UP: 1, RIGHT: 2,
+                             DOWN: 3, LEFT: 4}
+        self.tile.rotate()
+        self.assertEqual(self.tile.matches[UP], 2)
+        self.assertEqual(self.tile.matches[RIGHT], 3)
+        self.assertEqual(self.tile.matches[DOWN], 4)
+        self.assertEqual(self.tile.matches[LEFT], 1)
+
+    def test_flip_changes_matches(self):
+        self.tile.matches = {UP: 1, RIGHT: 2,
+                             DOWN: 3, LEFT: 4}
+        self.tile.flip()
+        self.assertEqual(self.tile.matches[UP], 3)
+        self.assertEqual(self.tile.matches[RIGHT], 2)
+        self.assertEqual(self.tile.matches[DOWN], 1)
+        self.assertEqual(self.tile.matches[LEFT], 4)
+
     def test_sides(self):
         self.assertEqual(
             self.tile.sides(),
-            ('..#', '###', '.##', '.#.')
+            ('..#.', '.#..', '#.#.', '.#.#')
         )
 
     def test_reversed_sides(self):
         self.assertEqual(
             self.tile.reversed_sides(),
-            ('#..', '###', '##.', '.#.')
+            ('.#..', '..#.', '.#.#', '#.#.')
         )
 
     def test_all_sides(self):
         self.assertEqual(
             self.tile.all_sides(),
-            ('..#', '###', '.##', '.#.', '#..', '###', '##.', '.#.')
+            ('..#.', '.#..', '#.#.', '.#.#', '.#..', '..#.', '.#.#', '#.#.')
         )
+
+    def test_sides_match(self):
+        other = Tile(2, ["....", "....", "....", "..#."])
+        other2 = Tile(3, ["....", "....", "....", ".#.."])
+        self.assertTrue(self.tile.sides_match(other, "upside"))
+        self.assertFalse(self.tile.sides_match(other2, "upside"))
+
+    def test_image_data(self):
+        expected = [["#", "."], [".", "."]]
+        data = self.tile.image_data()
+        for x in range(2):
+            for y in range(2):
+                self.assertEqual(data[x][y], expected[x][y])
+
+
+IMAGE = """.#.#..#.##...#.##..#####
+###....#.#....#..#......
+##.##.###.#.#..######...
+###.#####...#.#####.#..#
+##.#....#.##.####...#.##
+...########.#....#####.#
+....#..#...##..#.#.###..
+.####...#..#.....#......
+#..#.##..#..###.#.##....
+#.####..#.####.#.#.###..
+###.#.#...#.######.#..##
+#.####....##..########.#
+##..##.#...#...#.#.#.#..
+...#..#..#.#.##..###.###
+.#.#....#.##.#...###.##.
+###.#...#..#.##.######..
+.#.#.###.##.##.#..#.##..
+.####.###.#...###.#..#.#
+..#.#..#..#.#.#.####.###
+#..####...#.#.#.###.###.
+#####..#####...###....##
+#.##..#..#...#..####...#
+.#.###..##..##..####.##.
+...###...##...#...#..###
+"""
+
+
+class TestMain(unittest.TestCase):
+
+    def test_monster_search(self):
+        tile = self._create_test_tile()
+        self.assertEqual(sea_monster_count(tile, MONSTER), 2)
+
+    @staticmethod
+    def _create_test_tile():
+        data = numpy.array([list(row) for row in IMAGE.splitlines()])
+        tile = Tile(1, data)
+        return tile
+
+    def test_corner_tiles(self):
+        _, corner_tiles = self._get_tiles_corner_tiles()
+        for corner_id in [1951, 3079, 2971, 1171]:
+            self.assertTrue(corner_id in [tile.id for tile in corner_tiles])
+
+    @staticmethod
+    def _get_tiles_corner_tiles():
+        tiles = read_input("test_input")
+        search_matches(tiles)
+        corner_tiles = search_corners(tiles)
+        return tiles, corner_tiles
+
+    def test_create_image(self):
+        tiles, corner_tiles = self._get_tiles_corner_tiles()
+        corner_tiles[0].flip()
+        image = create_image(corner_tiles, 3)
+        # 1951, 2311, 3079
+        # 2729, 1427, 2473
+        # 2971, 1489, 1171
+        self.assertEqual(image[0, 0].id, 1951)
+        self.assertEqual(image[0, 1].id, 2729)
+        self.assertEqual(image[0, 2].id, 2971)
+        self.assertEqual(image[1, 0].id, 2311)
+        self.assertEqual(image[1, 1].id, 1427)
+        self.assertEqual(image[1, 2].id, 1489)
+        self.assertEqual(image[2, 0].id, 3079)
+        self.assertEqual(image[2, 1].id, 2473)
+        self.assertEqual(image[2, 2].id, 1171)
+
+    def test_original_image(self):
+        tiles, corner_tiles = self._get_tiles_corner_tiles()
+        image_tile = original_image(tiles, corner_tiles)
+        for side in self._create_test_tile().sides():
+            self.assertTrue(side in image_tile.all_sides())
